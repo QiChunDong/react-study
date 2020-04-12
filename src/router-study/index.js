333098f964f92a65aa34ccc43e1595f4d18943ca
齐春东 <qichundong@ebchinatech.com>
2020-04-12 16:58:56 +0800

    路由传递参数

--- src/router-study/index.js
+++ src/router-study/index.js
@@ -14,16 +14,18 @@ class comp extends React.Component{
 
     // Link 类似a标签 可以指定跳转的页面
     // Route是具体的路由配置 所路由到的页面也会在这个标签渲染类似于一个容器
+    // 路由的传参类似于vue的router 可以在path上定义参数 也买你通过props接收参数
     render() {
         return (
             <Router>
                 <div>
                     <Link to="/">首页</Link>&nbsp;
-                    <Link to="/comp1">组件1</Link>&nbsp;
+                    <Link to="/comp1/1">组件1</Link>&nbsp;
+                    <Link to="/comp1/2">组件12</Link>&nbsp;
                     <Link to="/comp2">组件2</Link>
                     
                     <Route path="/" exact component={Home}></Route>
-                    <Route path="/comp1"  component={RouterStuComp1}></Route>
+                    <Route path="/comp1/:id"  component={RouterStuComp1}></Route>
                     <Route path="/comp2" component={RouterStuComp2}></Route>
                 </div>
             </Router>
--- src/router-study/routerStuComp1.js
+++ src/router-study/routerStuComp1.js
@@ -4,18 +4,33 @@ class comp extends React.Component{
     constructor(...args) {
         super(...args);
         this.state = {
-            id: 0
+            title: '大一新生'
         }
     }
 
     componentDidUpdate (old_props, old_state) {
-        console.log('update')
+        // 首先 更改了路由参数不会重新渲染页面
+        // 其次 不能直接用state来暂存props里的参数 然后在此钩子里进行差异比对
+        // 原因是会造成冗余的钩子触发
+        // 所以此处应该用旧的props和新的props来比对路由参数的变化
+        // 因为此处可能会有异步请求，较为消耗资源 所以不允许冗余触发
+
+        // 路由参数可以通过props里的match或者location进行获取 常用的是match
+        let newId = this.props.match.params.id
+        let oldId = old_props.match.params.id
+        console.log(newId, oldId)
+        // 然后进行对比 有差异再更新state
+        if (newId != oldId) {
+            this.setState({
+                title: newId === '1' ? '大一新生' : '大四老油条'
+            })
+        }
     }
 
     render() {
         return (
             <div>
-                组件： {this.state.id}
+                组件： {this.state.title}
                 <div>
                     最后的最后
                 </div>
